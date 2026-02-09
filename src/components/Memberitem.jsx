f762f93e1e7cd49930d844f929098a53f9bfd404
429 <a429@MacBook-Pro.local>
2026-02-09 14:07:58 +0200

    fixed memberships component

--- src/components/Memberitem.jsx
+++ src/components/Memberitem.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { createPortal } from 'react-dom'
 import Yrii from '../pict/lobbyPict/MaksDjygun.webp'
+import linkedin from '../pict/linkedin.svg'
+import fb from '../pict/facebook.svg'
+import insta from '../pict/insta.svg'
 import '../css/memberitem.css'
 
 const Memberitem = ({ img, name, position, role, description }) => {
+    const [showText, setShowText] = React.useState(false);
+    const clickText = () => {
+        setShowText(!showText);
+    }
+    const closeText = (e) => {
+        e.stopPropagation();
+        setShowText(false);
+    }
     return (
-        <div className='memberitem'>
+        <div className='memberitem' onClick={clickText}>
             <div className='memberitem__img-wrapper'>
                 <img className='memberitem__img' src={img} alt="" />
             </div>
@@ -22,10 +34,25 @@ const Memberitem = ({ img, name, position, role, description }) => {
                     {description}
                 </p>
             </div>
-
-
-
-
+            {showText && <div className='modalblock' onClick={closeText}>
+                <div className='modalblock__content'>
+                    <button className="close-btn" onClick={closeText}>+</button>        
+                    <div className='modalblock__img'>
+                        <img src={img} alt="" />
+                    </div>
+                    <div className='modalblock__text'>
+                        <h3>{name}</h3>
+                        <p>{position}</p>
+                        <p>{role}</p>
+                        <p>{description}</p>
+                        <div className='modalblock__social-links'>
+                            <a href=""><img src={linkedin} alt="Image" /></a>
+                            <a href=""><img src={fb} alt="Image" /></a>
+                            <a href=""><img src={insta} alt="Image" /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>}
         </div>
     )
 }
